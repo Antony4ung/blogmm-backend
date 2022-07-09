c93c0061904e5aa6afdf861e455925512454f695
Aung Myat Thu <thua0524@gmail.com>
2022-07-09 21:39:46 +0700

    fin

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,45 +7,46 @@ const {
   generateToken,
 } = require("../utils/helperFunctions");
 
+const bcrypt = require("bcryptjs");
 const loginController = async (req, res) => {
   // try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-      res.status(400).json({ message: "Invalid Credentials" });
-      return;
-    }
-
-    const userExist = await user.findOne({ email: email });
-
-    if (!userExist) {
-      res.status(400).json({ message: "Invalid Credentials" });
-      return;
-    }
-
-    const isPasswordMatch = await comparePassword(password, userExist.password);
-
-    if (!isPasswordMatch) {
-      res.status(400).json({ message: "Invalid Password" });
-      return;
-    }
-
-    const {isAdmin,_id,name,photoUrl} = userExist
-
-    const token = await generateToken(email,password,isAdmin,_id,name);
-
-    res.status(200).json({
-      message: "Login success",
-      success: true,
-      token: token,
-      user: {
-        id:_id,
-        name: name,
-        email: email,
-        photoUrl:photoUrl
-      },
-    });
+  if (!email || !password) {
+    res.status(400).json({ message: "Invalid Credentials" });
     return;
+  }
+
+  const userExist = await user.findOne({ email: email });
+
+  if (!userExist) {
+    res.status(400).json({ message: "Invalid Credentials" });
+    return;
+  }
+
+  const isPasswordMatch = await comparePassword(password, userExist.password);
+
+  if (!isPasswordMatch) {
+    res.status(400).json({ message: "Invalid Password" });
+    return;
+  }
+
+  const { isAdmin, _id, name, photoUrl } = userExist;
+
+  const token = await generateToken(email, password, isAdmin, _id, name);
+
+  res.status(200).json({
+    message: "Login success",
+    success: true,
+    token: token,
+    user: {
+      id: _id,
+      name: name,
+      email: email,
+      photoUrl: photoUrl,
+    },
+  });
+  return;
   // } catch (error) {
   //   res.status(500).json(error);
   //   return;
@@ -53,103 +54,93 @@ const loginController = async (req, res) => {
 };
 
 const registerController = async (req, res) => {
-  
   try {
-   
-
-    const { name, email, password,rePassword,photoUrl } = req.body;
 
 
-    // const isEmail = validateEmail(email);
+  const { name, email, password, rePassword, photoUrl } = req.body;
 
-    // if (!isEmail) {
-    //   res.status(400).json({ message: "Invalid Email" });
-    //   return;
-    // }
+  const isEmailAlreadyExist = await user.findOne({ email: email });
 
-    const isEmailAlreadyExist = await user.findOne({ email: email });
-
-    if (isEmailAlreadyExist) {
-      res.status(401).json({ message: "User already exists" });
-      return;
-    }
-    
-    if(password !== rePassword){
-      res.status(401).json({ message: "Password is not match" });
-      return;
-    }
-
-    const hashedPassword = await hashPassword(password);
-
-
-    const newUser = new user({
-      name: name,
-      email: email,
-      password: hashedPassword,
-      photoUrl:photoUrl
-    });
-
-    const userStatus = await newUser.save();
-
-    res.status(201).json({ message: "User created",success:true });
+  if (isEmailAlreadyExist) {
+    res.status(401).json({ message: "User already exists" });
     return;
+  }
+
+  if (password !== rePassword) {
+    res.status(401).json({ message: "Password is not match" });
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new user({
+    name: name,
+    email: email,
+    password: hashedPassword,
+    photoUrl: photoUrl,
+  });
+
+  const userStatus = await newUser.save();
+
+  res.status(201).json({  success: true });
+  return;
   } catch (error) {
     res.status(500).json(error);
     return;
   }
 };
 
-
-
-
-const resetPassword = async (req,res) =>{
+const resetPassword = async (req, res) => {
   try {
-    const {email,password,newPassword} = req.body 
+    const { email, password, newPassword } = req.body;
 
-    const userFound = await user.findOne({email:email})
+    const userFound = await user.findOne({ email: email });
 
-    if(!userFound){
+    if (!userFound) {
       res.status(400).json({ message: "Account Not Found" });
       return;
     }
 
     const passwordIsMatch = await comparePassword(password, userFound.password);
 
-    if(!passwordIsMatch){
-      res.status(400).json({ message: "Password is not match and can't update user data" });
+    if (!passwordIsMatch) {
+      res
+        .status(400)
+        .json({ message: "Password is not match and can't update user data" });
       return;
     }
 
-    const hashedPassword = await hashPassword(newPassword)
+    const hashedPassword = await hashPassword(newPassword);
 
-    const status = await user.findOneAndUpdate({email:email},{password:hashedPassword},{new:true})
+    const status = await user.findOneAndUpdate(
+      { email: email },
+      { password: hashedPassword },
+      { new: true }
+    );
 
-    res.status(201).json({success:true,message:"Password Updated"})
-
+    res.status(201).json({ success: true, message: "Password Updated" });
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
     return;
   }
-}
+};
 
-const deleteAccount = async (req,res) =>{
+const deleteAccount = async (req, res) => {
   try {
-    const {email} = req.body
+    const { email } = req.body;
 
-    const userToDelete = await user.findOneAndDelete({email:email})
+    const userToDelete = await user.findOneAndDelete({ email: email });
 
-    res.status(202).json({success:true,message:"Account Deleted"})
-
-
+    res.status(202).json({ success: true, message: "Account Deleted" });
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
     return;
   }
-}
+};
 
 module.exports = {
   loginController,
   registerController,
   resetPassword,
-  deleteAccount
+  deleteAccount,
 };
